a2c0a7f8e7f6fcb18cd1f87fc3482bf895c3990e
Sahashra Rajbhandari <52586276+Sahashra-Rajbhandari@users.noreply.github.com>
2019-07-23 17:45:24 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,32 @@
+import { songInfo } from '../../app/songInfo';
+
+export const mySongs: songInfo[] = [
+    {
+        song: "Falling In Love With You",
+        artist: "Kina Grannis",
+        genre: "Singer-Songwriter",
+        year: 2018,
+        picture: "../assets/Images/Filwy.jpg"
+    },
+    {
+        song: "As Long As You Love Me",
+        artist: "Backstreet Boys",
+        genre: "Pop",
+        year: 1997,
+        picture: "../assets/Images/Alaylm.jpg"
+    },
+    {
+        song: "The Night We Met",
+        artist: "Lord Huron",
+        genre: "Alternative/Indie, Rock",
+        year: 2015,
+        picture: "../assets/Images/Tnwm.jpg"
+    },
+    {
+        song: "All I Want",
+        artist: "Kodaline",
+        genre: "Alternative/Indie",
+        year: 2013,
+        picture: "../assets/Images/Aiw.jpg"
+    }
+]
---
+++ assets/data/rajbhansClass.ts
@@ -0,0 +1,10 @@
+import { rajbhans } from '../../app/rajbhans';
+
+export const rajbhansClass: rajbhans = 
+    {
+        snum: 991478150,
+        sname: "Sahashra Rajbhandari",
+        slogin: "rajbhans",
+        scampus: "Davis Campus",
+        assigntitle: "rajbhansA3"
+    }
