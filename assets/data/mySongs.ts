3f686b952730271d75c4aa28b2bc7e9872b01fbe
Sahashra Rajbhandari <52586276+Sahashra-Rajbhandari@users.noreply.github.com>
2019-07-23 18:55:13 -0400

    update location
    Change the location of the image.

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -6,27 +6,27 @@ export const mySongs: songInfo[] = [
         artist: "Kina Grannis",
         genre: "Singer-Songwriter",
         year: 2018,
-        picture: "../assets/Images/Filwy.jpg"
+        picture: "./assets/Images/Filwy.jpg"
     },
     {
         song: "As Long As You Love Me",
         artist: "Backstreet Boys",
         genre: "Pop",
         year: 1997,
-        picture: "../assets/Images/Alaylm.jpg"
+        picture: "./assets/Images/Alaylm.jpg"
     },
     {
         song: "The Night We Met",
         artist: "Lord Huron",
         genre: "Alternative/Indie, Rock",
         year: 2015,
-        picture: "../assets/Images/Tnwm.jpg"
+        picture: "./assets/Images/Tnwm.jpg"
     },
     {
         song: "All I Want",
         artist: "Kodaline",
         genre: "Alternative/Indie",
         year: 2013,
-        picture: "../assets/Images/Aiw.jpg"
+        picture: "./assets/Images/Aiw.jpg"
     }
 ]
